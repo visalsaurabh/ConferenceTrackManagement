57fe9ef85ad6ba2233f99e3e4afccc5423849139
visalsaurabh <saurabhpv@hotmail.com>
2015-11-30 15:36:56 +0530

    Conference Track Management
    You are planning a big programming conference and have received many
    proposals which have passed the initial screen process but you're having
    trouble fitting them into the time constraints of the day -- there are
    so many possibilities! So you write a program to do it for you.
    The conference has multiple tracks each of which has a morning and
    afternoon session.
    Each session contains multiple talks.
    Morning sessions begin at 9am and must finish by 12 noon, for lunch.
    Afternoon sessions begin at 1pm and must finish in time for the
    networking event.
    The networking event can start no earlier than 4:00 and no later than
    5:00.
    No talk title has numbers in it.
    All talk lengths are either in minutes (not hours) or lightning (5
    minutes).
    Presenters will be very punctual; there needs to be no gap between
    sessions.
    
    Note that depending on how you choose to complete this problem, your
    solution may give a different ordering or combination of talks into
    tracks.

---
+++ ConferenceTrackManagement/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace ConferenceTrackManagement
+{
+    public class Talk
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public Boolean IsSchedule { get; set; }
+
+        public void Getduration(Talk[] talks, String[] lines)
+        {
+            for (int i = 0; i < talks.Length; i++)
+            {
+                talks[i] = new Talk();
+                talks[i].Name = lines[i]; ;
+                talks[i].IsSchedule = false;
+                string[] items = lines[i].Trim().Split(' ');
+                int len2 = items.Length - 1;
+                string last = items[len2].ToLower();
+                if (last == "lightning")
+                {
+                    talks[i].Duration = 5;
+                }
+                else
+                {
+                    talks[i].Duration = int.Parse(last.Replace("min", ""));
+                }
+            }
+        }
+
+        public void sortDisplay(Talk[] talks)
+        {
+            List<Talk> track1am = new List<Talk>();
+            List<Talk> track1pm = new List<Talk>();
+            List<Talk> track2am = new List<Talk>();
+            List<Talk> track2pm = new List<Talk>();
+
+            int CheckedAllSession = 0;
+
+            talks = talks.OrderByDescending(t => t.Duration).ToArray();
+
+            int schedule1am = 0, schedule1pm = 0, schedule2am = 0, schedule2pm = 0;
+            int next = 0;
+
+            for (int i = 0; i < talks.Length; i++)
+            {
+                int duration = talks[i].Duration;
+
+                if (next == 0 && (schedule1am + duration) <= 180)
+                {
+                    track1am.Add(talks[i]);
+                    talks[i].IsSchedule = true;
+                    schedule1am += duration;
+                    next++;
+                    CheckedAllSession = 0;
+                }
+                else if (next == 1 && (schedule2am + duration) <= 180)
+                {
+                    track2am.Add(talks[i]);
+                    talks[i].IsSchedule = true;
+                    schedule2am += duration;
+                    next++;
+                    CheckedAllSession = 0;
+                }
+                else if (next == 2 && (schedule1pm + duration) <= 240)
+                {
+                    track1pm.Add(talks[i]);
+                    talks[i].IsSchedule = true;
+                    schedule1pm += duration;
+                    next++;
+                    CheckedAllSession = 0;
+                }
+                else if (next == 3 && (schedule2pm + duration) <= 240)
+                {
+                    track2pm.Add(talks[i]);
+                    talks[i].IsSchedule = true;
+                    schedule2pm += duration;
+                    next = 0;
+                    CheckedAllSession = 0;
+                }
+                else
+                {
+                    i--;
+                    next++;
+                    if (next == 4) next = 0;
+                    CheckedAllSession++;
+                    if (CheckedAllSession == 4)
+                    {
+                        i++;
+                        CheckedAllSession = 0;
+                    }
+                }
+            }
+
+            int mins = Math.Max(schedule1pm, schedule2pm);
+            if (mins < 180) 
+                mins = 180; // can't be before 4.00 pm
+            if (mins > 180) 
+                mins = 240;
+            DateTime nwe = DateTime.Today.AddHours(13).AddMinutes(mins);
+
+            // print results
+            Console.Clear();
+            Console.WriteLine("Track1:");
+            DateTime dt = DateTime.Today.AddHours(9);
+
+
+            foreach (Talk t in track1am)
+            {
+                Console.WriteLine("{0} {1}", dt.ToString("hh:mmtt"), t.Name);
+                dt = dt.AddMinutes(t.Duration);
+            }
+
+            Console.WriteLine("12:00PM Lunch");
+            dt = DateTime.Today.AddHours(13);
+
+            foreach (Talk t in track1pm)
+            {
+                Console.WriteLine("{0} {1}", dt.ToString("hh:mmtt"), t.Name);
+                dt = dt.AddMinutes(t.Duration);
+            }
+
+            Console.WriteLine("{0} Networking Event", nwe.ToString("hh:mmtt"));
+
+            Console.WriteLine("\nTrack2:");
+            dt = DateTime.Today.AddHours(9);
+
+            foreach (Talk t in track2am)
+            {
+                Console.WriteLine("{0} {1}", dt.ToString("hh:mmtt"), t.Name);
+                dt = dt.AddMinutes(t.Duration);
+            }
+
+            Console.WriteLine("12:00PM Lunch");
+            dt = DateTime.Today.AddHours(13);
+
+            foreach (Talk t in track2pm)
+            {
+                Console.WriteLine("{0} {1}", dt.ToString("hh:mmtt"), t.Name);
+                dt = dt.AddMinutes(t.Duration);
+            }
+
+            Console.WriteLine("{0} Networking Event", nwe.ToString("hh:mmtt"));
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Un scheduled Event");
+
+            foreach (Talk t in talks)
+            {
+                if (t.IsSchedule == false)
+                {
+                    Console.WriteLine(t.Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+  
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(@"InputData\schedule.txt");
+                int len = lines.Length;
+                Talk[] talks = new Talk[len];
+
+                Talk talk = new Talk();
+                talk.Getduration(talks,lines);
+                talk.sortDisplay(talks);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong.Please check");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+Steps to run project?
+
+     1.Navigate to ConferenceTrackManagement folder.
+
+     2.Then click on ConferenceTrackManagement.csproj file.This will automatically 
+       start visual studio with ConferenceTrackManagement project.
+     
+     3.After starting visual studio you can see top navigation bar.
+
+     4.Navigate to the Build menu and then select
+       Build-> Build Solution.
+
+     5.After the build completes navigate to Debug menu and select
+       Debug-> Start Debugging.
+       or
+       Debug-> Start Without Debugging.
+
+     6.By doing above steps program will be run and output will be shown in black screen.
+
+     7.To come out of black screen or to stop the execution just press any key on the key-
+       board twice or hit the close button which will be seen on the top right hand side
+       of the black window.
+      
+     Note:- I have kept sample input for the program in a folder called InputData and the 
+	name of the file is schedule.txt.
+      
